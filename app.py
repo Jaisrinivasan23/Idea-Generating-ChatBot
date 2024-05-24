57e17e2872fe0a0fa1cc50b57d1849a79d20f13c
Jaisrinivasan J <137684850+Jaisrinivasan23@users.noreply.github.com>
2024-05-24 09:06:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 # Replace 'your_replicate_api_token' with your actual Replicate API token
-REPLICATE_API_TOKEN = 'r8_YzwhBsALLuYoQLe2mpIIyAmBqDKjm3d0uhCLs'
+REPLICATE_API_TOKEN = 'your_replicate_api_token'
 os.environ['REPLICATE_API_TOKEN'] = REPLICATE_API_TOKEN
 
 # Default model configuration
